e9ef46620fda2afe1a5a474b63c455cec0db17e4
Yunus Emre Deligöz <ye@deligoz.me>
2021-12-05 22:43:30 +0300

    SentenceGeneratorTest: Add `it_can_generate_desired_number_of_sentences()` test

--- tests/SentencesGeneratorTest.php
+++ tests/SentencesGeneratorTest.php
@@ -9,8 +9,22 @@ class SentencesGeneratorTest extends BaseTestCase
     /** @test */
     public function it_can_generate_multiple_sentences(): void
     {
-        $sentences = static::$generator->sentences(10);
+        // Act
+        $sentences = static::$generator->sentences();
 
+        // Assert
+        expect($sentences)
+            ->toBeArray()
+            ->each()->toBeString();
+    }
+
+    /** @test */
+    public function it_can_generate_desired_number_of_sentences(): void
+    {
+        // Act
+        $sentences = static::$generator->sentences(numberOfSentences: 10);
+
+        // Assert
         expect($sentences)
             ->toBeArray()
             ->toHaveLength(10);
