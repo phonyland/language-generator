099154d255dcc07fc1d52d85ddbbfb4f6162026a
Yunus Emre Deligöz <ye@deligoz.me>
2021-12-05 13:58:43 +0300

    GeneratorException: Add `invalidNGramLength()` Exception

--- src/Exceptions/GeneratorException.php
+++ src/Exceptions/GeneratorException.php
@@ -7,4 +7,10 @@ use Phonyland\LanguageGenerator\Generator;
 
 class GeneratorException extends Exception
 {
+    public static function invalidNGramLength(Generator $generator): self
+    {
+        return new static(
+            message: "First n-Gram lenght must equal to {$generator->modelData['config']['n']} for this model."
+        );
+    }
 }
