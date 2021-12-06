9124f304bd7c366359d0de0c99dab3f32b254033
Yunus Emre Deligöz <ye@deligoz.me>
2021-12-06 09:51:59 +0300

    ParagraphGeneratorTest: Add `it_can_generate_a_paragraph_with_desired_number_of_sentences()` test

--- tests/ParagraphGeneratorTest.php
+++ tests/ParagraphGeneratorTest.php
@@ -9,8 +9,23 @@ class ParagraphGeneratorTest extends BaseTestCase
     /** @test */
     public function it_can_generate_a_paragraph(): void
     {
-        $paragraph = static::$generator->paragraph(10);
+        // Act
+        $paragraph = static::$generator->paragraph();
 
+        // Assert
         expect($paragraph)->toBeString();
+    }
+
+    /** @test */
+    public function it_can_generate_a_paragraph_with_desired_number_of_sentences(): void
+    {
+        // Act
+        $paragraph = static::$generator->paragraph(
+            numberOfSentences: 10,
+            endingPunctuation: '#',
+        );
+
+        // Assert
+        expect(explode('# ', $paragraph))->toHaveCount(10);
     }
 }
