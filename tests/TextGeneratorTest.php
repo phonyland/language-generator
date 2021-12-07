252f3bc57ed41143396798b58dee1e77deae809e
Yunus Emre Deligöz <ye@deligoz.me>
2021-12-07 22:48:55 +0300

    Generator: Add `it_can_generate_a_text_with_desired_sentence_ending_punctuations()` test

--- tests/TextGeneratorTest.php
+++ tests/TextGeneratorTest.php
@@ -30,6 +30,24 @@ class TextGeneratorTest extends BaseTestCase
     }
 
     /** @test */
+    public function it_can_generate_a_text_with_desired_sentence_ending_punctuations(): void
+    {
+        // Arrange
+        $sentenceEndingPunctuation = ['###', '@@@'];
+
+        // Act
+        $text = static::$generator->text(
+            maxNumberOfCharacters: 1000,
+            sentenceEndingPunctuation: $sentenceEndingPunctuation
+        );
+
+        // Assert
+        expect($text)
+            ->toContain($sentenceEndingPunctuation[0])
+            ->toContain($sentenceEndingPunctuation[1]);
+    }
+
+    /** @test */
     public function it_can_generate_a_text_with_desired_number_of_characters_and_a_suffix(): void
     {
         // Arrange
