603baa086046d323e3989a18f00abb75007e5c91
Yunus Emre Deligöz <ye@deligoz.me>
2021-12-10 00:26:55 +0300

    PoemGeneratorTest: Add `it_can_generate_a_poem_with_desired_stanza_length()` test

--- tests/PoemGeneratorTest.php
+++ tests/PoemGeneratorTest.php
@@ -33,4 +33,21 @@ class PoemGeneratorTest extends BaseTestCase
             ->toHaveCount($numberOfVerses)
             ->each()->toBeString();
     }
+
+    /** @test */
+    public function it_can_generate_a_poem_with_desired_stanza_length(): void
+    {
+        // Arrange
+        $numberOfVerses = 120;
+        $stanzaLenght = random_int(2, 5);
+
+        // Act
+        $poem = static::$generator->poem(
+            numberOfVerses: $numberOfVerses,
+            stanzaLength: $stanzaLenght,
+        );
+
+        expect(explode(PHP_EOL . PHP_EOL, $poem))
+            ->toHaveCount($numberOfVerses / $stanzaLenght);
+    }
 }
